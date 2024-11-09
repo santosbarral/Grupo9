b084644f5c4d2868e4e143d133d0776fd3932d6e
santosbarral <sbarral@udesa.edu.ar>
2024-11-09 17:04:27 -0300

    Modelo User

---
+++ myapp/database/models/User.js
@@ -0,0 +1,35 @@
+module.exports =  function(sequelize, dataTypes) {
+
+    let alias = "User";
+
+    let cols = {
+        id : {
+            autoIncrement : true,
+            primaryKey : true,
+            type : dataTypes.INTEGER
+        },
+        name : {
+            type : dataTypes.STRING
+        },
+        email : {
+            type : dataTypes.STRING
+        },
+        password : {
+            type : dataTypes.STRING
+        },
+        remember_token : {
+            type : dataTypes.STRING
+        },
+    };
+
+    let config = {
+        tableName : "users",
+        timestamps : false,
+        underscored : true
+    }
+
+    let User = sequelize.define(alias, cols, config);
+
+    return User;
+    
+}
