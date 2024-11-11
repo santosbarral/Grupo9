0e487d1537b221e8f1bd47faba3c70cd2f0cdd1d
santosbarral <sbarral@udesa.edu.ar>
2024-11-11 09:32:54 -0300

    gitignore

---
+++ myapp/database/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  "development": {
+    "username": "root",
+    "password": "root",
+    "database": "movies_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+
+module.exports = config;
