55e8d062cae5273dbb4ca1a3bbee830ead948edc
martinaFCovaro <mfernandezcovaro@gmail.com>
2024-11-11 17:17:51 -0300

    termine el logout y el login casi entero

--- myapp/controllers/userController.js
+++ myapp/controllers/userController.js
@@ -12,16 +12,25 @@ const userController = {
         let form = req.body;
 
         if (form.email == "") {
-            return res.send('Completar email')
-
-            
+            return res.send('Completar email')    
         }else if (form.password == "") {
             return res.send('Completar la contraseña')
         } else if (form.name =="") {
             return res.send('Completar el usuario')
         } else {
-            return res.redirect('/users/login');
-        }
+        form.password = bycryptjs.hashSync(form.password,10);
+        db.User.create(form)
+        .then(function (result) {
+            return res.redirect("/users/login");
+            
+        })
+        .catch(function (error) {
+            return console.log(error);
+            
+        });  
+        return res.redirect('/users/login');
+    }
+
     },
     loginPost: (req, res)=>{
         let form = req.body;
@@ -32,14 +41,14 @@ const userController = {
         .then((result) => {
             
             if (!result) {
-                return res.send('no hay mail')
+                return res.send('El email no se encuantra registrado')
             } else {        
                 let check = bycryptjs.compareSync(form.password , result.password)
                 if (check) {
                     req.session.user = result.dataValues
                     return res.redirect('/')
                 } else {
-                    return res.send('la contraseña es incorrecta')
+                    return res.send('La contraseña es incorrecta')
                 }
             }
         }).catch((err) => {
