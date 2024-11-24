9f14573d19d77328b52641e83cbf3903e14d8476
santosbarral <sbarral@udesa.edu.ar>
2024-11-24 15:22:01 -0300

    Arreglo de las imagenes de sql/Correcion Controllersy vistas de los products

--- myapp/controllers/productController.js
+++ myapp/controllers/productController.js
@@ -2,68 +2,63 @@ const { where } = require('sequelize');
 const db = require('../database/models');
 const op = db.Sequelize.Op;
 
-
 const productController = {
-    product:(req, res)=>{
-        
+    product: (req, res) => {
+        let filtrado = {
+            order: [["id", "DESC"]], 
+            limit: null // Si quieres traer todos los registros, usa 'null'
+        };
         db.Product.findAll(filtrado)
-        .then((result) =>{
-          return res.render("index", {listaProductos: result});
-        })
-        .catch((err) =>{
-          return console.log(err);
-        }) 
-        
-      },
-    
-    detalle: (req, res)=>{
-      let id = req.params.idProduct; //Del sistema de routeo :idProducto
-
-      db.Product.findByPk(id)
-      .then(function (results) {
-        return res.render('product',{product: results})
-      })
-      .catch(function (err) {
-        return console.log(err);
-
-      })
-      
-    },// Detalle lo dejo para despues  
-    showFormAdd: (req, res)=>{
-        return res.render("product-add"); 
-    },
-    store:(req, res)=>{
-      let product= req.body
-   
-      db.Product.create(product)
-      .then(function (results) {
-        
-        return res.redirect('/');
-      })
-      .catch(function (error) {
-        return console.log(error)
-      })    
+            .then((result) => {
+                return res.render("index", { listaProductos: result });
+            })
+            .catch((err) => {
+                return console.log(err);
+            });
     },
 
-    
+    detalle: (req, res) => {
+        let id = req.params.idProduct; // Del sistema de routeo :idProducto
 
-    search:(req, res)=>{
-      let qs = req.query.search;
-    let filtrado= {
-      where: [{productName: {[op.like]: `%${qs}%`}}],
-      order: [["createdAt", "DESC"]],
+        db.Product.findByPk(id)
+            .then(function (results) {
+                return res.render('product', { product: results });
+            })
+            .catch(function (err) {
+                return console.log(err);
+            });
+    },
 
+    showFormAdd: (req, res) => {
+        return res.render("product-add");
+    },
+
+    store: (req, res) => {
+        let product = req.body;
+
+        db.Product.create(product)
+            .then(function (results) {
+                return res.redirect('/');
+            })
+            .catch(function (error) {
+                return console.log(error);
+            });
+    },
+
+    search: (req, res) => {
+        let qs = req.query.search;
+        let filtrado = {
+            where: [{ productName: { [op.like]: `%${qs}%` } }],
+            order: [["createdAt", "DESC"]], // Ordenar también por fecha de creación descendente
+        };
+        db.Product.findAll(filtrado)
+            .then(function (results) {
+                return res.render("search-results", { productos: results });
+            })
+            .catch(function (error) {
+                return console.log(error);
+            });
     }
-    db.Product.findAll(filtrado)
-    .then(function (results) {
-      return res.render("search-results", {productos: results});
-    })
-    .catch(function (error) {
-      return console.log(error)
-    })
-    }
+};
 
-
-}
-    
-module.exports = productController
+module.exports = productController;
